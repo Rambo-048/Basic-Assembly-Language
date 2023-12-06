2275ee67486eb218b1179192bff4f81d9663bead
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2023-12-06 12:21:04 +0700

    Add files via upload

---
+++ 25 Print String Use INT 21h Service 09h/DOSBOX/String 1.asm
@@ -0,0 +1,20 @@
+;String.asm --> Print String ---> INT 21h Service 09h
+
+.MODEL
+.CODE
+ORG 100h
+
+TData: JMP Process
+String DB 'A B C D E $'
+Change DB 13,10,'$'
+Stacks DW
+
+Process:
+	 LEA DW, String		;BX=0
+	 PUSH DX
+	 MOV AH, 09		;Service for print Character
+	 INT 21h
+	
+Exit:	 INT 20h		;Exit
+
+END      TData
