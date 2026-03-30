1a1f83cf76fabc7e4d744c384593786145ae842e
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-03-30 17:51:00 +0700

    Update memory model to include MEDIUM

--- 18 Assembly Language Print Char/DOSBOX/Char.asm
+++ 18 Assembly Language Print Char/DOSBOX/Char.asm
@@ -1,11 +1,11 @@
 ;Char.asm --> PRINT CHAR (.COM)
 ;CREATE AND COMPILE USE DOSBOX
 
-.MODEL                                   ;TINY,SMALL,LARGE,HUGE
+.MODEL                                   ;TINY,SMALL,MEDIUM,LARGE,HUGE
 .CODE
 ORG 100h
 
-Process:                                ;label or address
+Process:                            ;label or address
 	 MOV AX,02h                     ;value Service for print char
 	 MOV DL,'A'                     ;DL = print char ASCII
 	 INT 21h                        ;print char
