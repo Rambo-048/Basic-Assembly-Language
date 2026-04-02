6717e1f7edd0d8c8b76f2e44eaf8d1598b177212
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-04-02 10:58:48 +0700

    Update String3.asm

--- 23 Print String with Attribute/DOSBOX/String3.asm
+++ 23 Print String with Attribute/DOSBOX/String3.asm
@@ -11,13 +11,13 @@ String0 DB 'NEVER GIVE UP, BECAUSE GREAT THINGS TAKE TIME'
 Process:
 	MOV AX, 1300h		;Service 13h Subfunction 00
 	MOV BL, 10011010b	;Attribute String
-	MOV BH, 00		;PAGE SHOW 00
-	MOV DL, 40		;POSITION X
-	MOV DH, 20		;POSITION Y
-	MOV CX, 50		;TOTAL CHARACTER IN STRING
+	MOV BH, 00			;PAGE SHOW 00
+	MOV DL, 40			;POSITION X
+	MOV DH, 20			;POSITION Y
+	MOV CX, 50			;TOTAL CHARACTER IN STRING
 	LEA BP, String0		;ES:BP String Address ---> PRINT STRING ADDRESS
-	INT 10h			;Print String	
-	INT 20h			;exit
+	INT 10h				;Print String	
+	INT 20h				;exit
 
 END	DataString
 	
