219c0801eb6dcc2807a30a13180634a1b8635f0e
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-04-02 10:59:16 +0700

    Update String5.asm

--- 23 Print String with Attribute/DOSBOX/String5.asm
+++ 23 Print String with Attribute/DOSBOX/String5.asm
@@ -11,13 +11,13 @@ String0 DB 'STAY HOPEFUL YOU NEVER KNOW WHAT TOMORROW MAY BRING'
 Process:
 	MOV AX, 1300h		;Service 13h Subfunction 00
 	MOV BL, 10011010b	;Attribute String
-	MOV BH, 00		;PAGE SHOW 00
-	MOV DL, 60		;POSITION X
-	MOV DH, 20		;POSITION Y
-	MOV CX, 55		;TOTAL CHARACTER IN STRING
+	MOV BH, 00			;PAGE SHOW 00
+	MOV DL, 60			;POSITION X
+	MOV DH, 20			;POSITION Y
+	MOV CX, 55			;TOTAL CHARACTER IN STRING
 	LEA BP, String0		;ES:BP String Address ---> PRINT STRING ADDRESS
-	INT 10h			;Print String	
-	INT 20h			;exit
+	INT 10h				;Print String	
+	INT 20h				;exit
 
 END	DataString
 	
