b50c0bc5e72d2fda27acc926424553930da0c2ad
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-04-02 10:58:28 +0700

    Update String2.asm

--- 23 Print String with Attribute/DOSBOX/String2.asm
+++ 23 Print String with Attribute/DOSBOX/String2.asm
@@ -11,13 +11,13 @@ String0 DB 'SOMETIMES WE NEED FANTASY TO SURVIVE THE REALITY'
 Process:
 	MOV AX, 1300h		;Service 13h Subfunction 00
 	MOV BL, 00011000b	;Attribute String
-	MOV BH, 00		;PAGE SHOW 00
-	MOV DL, 30		;POSITION X
-	MOV DH, 15		;POSITION Y
-	MOV CX, 50		;TOTAL CHARACTER IN STRING
+	MOV BH, 00			;PAGE SHOW 00
+	MOV DL, 30			;POSITION X
+	MOV DH, 15			;POSITION Y
+	MOV CX, 50			;TOTAL CHARACTER IN STRING
 	LEA BP, String0		;ES:BP String Address ---> PRINT STRING ADDRESS
-	INT 10h			;Print String	
-	INT 20h			;exit
+	INT 10h				;Print String	
+	INT 20h				;exit
 
 END	DataString
 	
