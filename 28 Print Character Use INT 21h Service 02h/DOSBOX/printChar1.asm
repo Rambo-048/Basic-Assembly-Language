8078eac26b5d86dd6d05931f119f5bc25953a59f
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-04-03 12:03:43 +0700

    Add files via upload

---
+++ 28 Print Character Use INT 21h Service 02h/DOSBOX/printChar1.asm
@@ -0,0 +1,32 @@
+;printChar1.asm --> Print Char INT 21h Service 02h
+
+.MODEL SMALL
+.CODE
+ORG 100h
+
+DataString : JMP Process
+
+Process:
+	;PRINT CHAR
+	MOV AH,02h	;Service 02h for print char
+	MOV DL,'Z'	;INSERT CHAR TO DL
+	INT 21h		;PRINT CHAR
+	
+	;PRINT CHAR
+	MOV AH,02h	;Service 02h for print char
+	MOV DL,'?'	;INSERT CHAR TO DL
+	INT 21h		;PRINT CHAR
+	
+	;PRINT CHAR FROM ASCII
+	MOV DL,88	;INSERT ASCII CHAR TO DL
+	INT 21h		;PRINT CHAR
+	
+	;PRINT CHAR FROM ASCII
+	MOV DL,185	;INSERT ASCII CHAR TO DL
+	INT 21h		;PRINT CHAR
+
+Exit:
+	INT 20h
+
+END	DataString
+	
